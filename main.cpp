701543417ddc66ebc4caa943c441d77853e5a843
DavidN <deemunnyarkcali@gmail.com>
2026-02-02 17:46:48 -0800

    Fixed issue of not having any actual logic

--- main.cpp
+++ main.cpp
@@ -2,27 +2,38 @@
 #include "Student.h"
 #include <string>
 
+static int NUMSTUDENTS = 5;
+
 int main() {
-Student* s1 = new Student();
-Student* s2 = new Student();
-Student* s3 = new Student();
-Student* s4 = new Student();
-Student* s5 = new Student();
+
+Student* students[NUMSTUDENTS];
+
+for (int i = 0; i < NUMSTUDENTS; ++i) {
+    students[i] = new Student();
+}
 
 std::string tempString;
 int tempInt;
 
-for (int i = 0; i < 5; i++) {
-    std::cout << "Enter the name for student #" << i+1 << ":" << std::endl;
+// this should probably be a separate function outside of main
+for (int i = 0; i < NUMSTUDENTS; ++i) {
+    std::cout << "Enter the name for student #" << i+1 << ":";
     std::cin >> tempString;
-    std::cout << "Enter the age for student #" << i+1 << ":" << std::endl;
+    students[i]->setName(tempString);
+    std::cout << "Enter the age for student #" << i+1 << ":";
     std::cin >> tempInt;
+    students[i]->setAge(tempInt);
 }
 
-delete s1;
-delete s2;
-delete s3;
-delete s4;
-delete s5;
+for (int i = 0; i < NUMSTUDENTS; ++i) {
+    std::cout << "Student #" << i+1 << std::endl;
+    std::cout << "    Name: " << students[i]->getName() << std::endl;
+    std::cout << "    Age: "  << students[i]->getAge()  << std::endl << std::endl;
+}
+
+for (int i = 0; i < NUMSTUDENTS; i++)
+{
+    delete students[i];
+}
 
 }
